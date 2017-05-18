df169222050edc513f91925f7e7ae629e6e53201
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-18 15:31:28 +0200

    Add dprettier

--- test/RequestBuilder.js
+++ test/RequestBuilder.js
@@ -11,40 +11,27 @@ describe('RequestBuilder.js', () => {
   it('should be empty if nothing is set', () => {
     const requestBuilder = new RequestBuilder();
 
-    const actualA =
-      requestBuilder
-        .request()
-        .getOptions();
+    const actualA = requestBuilder.request().getOptions();
     const expectedA = {};
     assert.deepEqual(actualA, Object.assign({}, defaultOptions, expectedA));
 
-    const actualB =
-      requestBuilder
-        .request()
-        .getOptions();
+    const actualB = requestBuilder.request().getOptions();
     const expectedB = {};
     assert.deepEqual(actualB, Object.assign({}, defaultOptions, expectedB));
   });
 
   it('should be able to handle options by name for one request', () => {
-    const requestBuilder = new RequestBuilder()
-      .setOptions('google', {
-        url: 'http://google.de',
-      });
+    const requestBuilder = new RequestBuilder().setOptions('google', {
+      url: 'http://google.de',
+    });
 
-    const actualA =
-      requestBuilder
-        .request('google')
-        .getOptions();
+    const actualA = requestBuilder.request('google').getOptions();
     const expectedA = {
       url: 'http://google.de',
     };
     assert.deepEqual(actualA, Object.assign({}, defaultOptions, expectedA));
 
-    const actualB =
-      requestBuilder
-        .request()
-        .getOptions();
+    const actualB = requestBuilder.request().getOptions();
     const expectedB = {};
     assert.deepEqual(actualB, Object.assign({}, defaultOptions, expectedB));
   });
@@ -54,19 +41,13 @@ describe('RequestBuilder.js', () => {
       url: 'http://google.de',
     });
 
-    const actualA =
-      requestBuilder
-        .request()
-        .getOptions();
+    const actualA = requestBuilder.request().getOptions();
     const expectedA = {
       url: 'http://google.de',
     };
     assert.deepEqual(actualA, Object.assign({}, defaultOptions, expectedA));
 
-    const actualB =
-      requestBuilder
-        .request()
-        .getOptions();
+    const actualB = requestBuilder.request().getOptions();
     const expectedB = {
       url: 'http://google.de',
     };
